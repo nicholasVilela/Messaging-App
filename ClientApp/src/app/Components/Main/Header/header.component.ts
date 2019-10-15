d71b6404029c40148cf4c006b0b140ba5aee1ce1
Nicholas Vilela <nvilela@proforma.com>
2019-10-14 21:55:48 -0400

    added some extra css stuff

--- ClientApp/src/app/Components/Main/Header/header.component.ts
+++ ClientApp/src/app/Components/Main/Header/header.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/Services/firebase.service';
+import { SignalRService } from 'src/app/Services/signalR.service';
 
 @Component({
     selector: 'app-header',
-    templateUrl: 'header.component.html'
+    templateUrl: 'header.component.html',
+    styleUrls: ['./header.component.css'],
+    providers: [FirebaseService, SignalRService]
 })
 
 export class HeaderComponent implements OnInit {
